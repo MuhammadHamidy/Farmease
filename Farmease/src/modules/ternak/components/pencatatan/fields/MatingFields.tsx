1194ae1cb1e7bf37bf7e1080815ac4365dc9b063
m.hamidy <hamidym84@gmail.com>
2026-07-31 17:31:07 +0700

    fix: resolve JSX syntax error in MatingFields

--- Farmease/src/modules/ternak/components/pencatatan/fields/MatingFields.tsx
+++ Farmease/src/modules/ternak/components/pencatatan/fields/MatingFields.tsx
@@ -714,6 +714,7 @@ export default defineComponent({
                       </span>
                     </div>
                   </>
+                )}
               </div>
             </div>
           </div>
